247dec70d7442cbc707f9c8272b08e951eba1ff3
Anastasia <nstanastasia25@yandex.ru>
2022-06-08 13:01:45 +0300

    added task

---
+++ task/Program.cs
@@ -0,0 +1,20 @@
+﻿string str1 = "hello";
+string str2 = "new";
+string str3 = "day";
+string str4 = "Monday";
+if (str1.Length<4)
+{
+    Console.WriteLine(str1);
+}
+if (str2.Length<4)
+{
+    Console.WriteLine(str2);
+}
+if (str3.Length<4)
+{
+    Console.WriteLine(str3);
+}
+if (str4.Length<4)
+{
+    Console.WriteLine(str4);
+}
